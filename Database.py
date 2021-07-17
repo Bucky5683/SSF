4f8494673acada561acf33340a2bc1892f37490a
Bucky <87425043+Bucky5683@users.noreply.github.com>
2021-07-17 23:50:15 +0900

    ㅋㅋㅋㅋㅋㅋㅋ

--- Database.py
+++ Database.py
@@ -5,6 +5,7 @@ from pandas import Series, DataFrame
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
+
 def checkdatabase():
     # all_data 테이블이없으면 all_data 테이블 생성
     sql = "CREATE TABLE IF NOT EXISTS all_data(idx INTEGER PRIMARY KEY, title TEXT, age_min INTEGER, age_max INTEGER, area_code INTEGER, area_list TEXT, work TEXT, type TEXT, area TEXT, host TEXT, href TEXT)"
@@ -15,6 +16,7 @@ def checkdatabase():
     # youth 데이터 all_data로 이동
     conn.commit()
 
+
 def UpdateAlldata():
     cursor.execute("SELECT * FROM youth")
     youth = cursor.fetchall()
@@ -22,24 +24,54 @@ def UpdateAlldata():
     alldata = cursor.fetchall()
 
     index_num = []
-    check = True
 
+    if len(youth) > len(alldata):
+        InsertData(youth, alldata, index_num)
+    elif len(youth) < len(alldata):
+        DeleteData(youth, alldata, index_num)
+
+
+def InsertData(youth, alldata, index_num):
+    print("Insert Data")
+    check = True
+    # youth의 인덱스가 alldata보다 많을 경우 -> 새롭게 지원금 게시물이 올라온 경우
+    for i in youth:
+        check = True
+        for j in alldata:
+            if i == j:
+                check = False
+                break
+        if check == True:
+            print(i)
+            index_num.append(i[0])
+
+    for insertion in index_num:
+        cursor.execute('SELECT * FROM youth WHERE index="%d" ')
+        insert_data = cursor.fetchone()
+        sql = 'INSERT INTO all_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        print(cursor.execute(sql, insert_data))
+    print(index_num)
+
+
+def DeleteData(youth, alldata, index_num):
+    print("Delete Data")
+    check = True
+    # youth의 인덱스가 alldata보다 적을 경우 -> 지원금게시물이 하나 삭제된 경우
     for i in alldata:
         check = True
         for j in youth:
             if i == j:
                 check = False
                 break
-        if check==True:
+        if check == True:
             print(i[0])
             index_num.append(i[0])
 
     print(index_num)
 
-    #cursor.execute("INSERT INTO all_data SELECT * FROM youth;")
+    # cursor.execute("INSERT INTO all_data SELECT * FROM youth;")
+
 
 checkdatabase()
 UpdateAlldata()
 conn.commit()
-
-
--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, session
 import pandas as pd
+from flask_login import LoginManager
 from pandas import Series, DataFrame
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 
 # 루트
@@ -27,18 +32,22 @@ def youth():
 
 
 # 제목, 링크, 나이, 직업, 소득분위, 지원금TF json 코드 str형태로 리턴
-@app.route("/testing")
+@app.route("/jsoncode")
 def TodoPost():
     con = sqlite3.connect("database.db")
     cur = con.cursor()
     query = cur.execute("SELECT * From youth")
     cols = [column[0] for column in query.description]
     race_result = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    title = race_result[['title', 'href', 'age', 'work', 'rank', 'type']]
+    title = race_result[['index', 'title', 'href', 'age_min', 'age_max', 'work', 'area', 'type', 'host']]
     js = title.to_json(orient='records')
     print(type(js))
+    print(title)
     return js
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 if __name__ == '__main__':
