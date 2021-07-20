0d32c6d96263ca1ea78320077c09dd53198912f0
Bucky <bucky5683@gmail.com>
2021-07-20 23:57:12 +0900

    upload

--- Database.py
+++ Database.py
@@ -8,7 +8,7 @@ cursor = conn.cursor()
 
 def checkdatabase():
     # all_data 테이블이없으면 all_data 테이블 생성
-    sql = "CREATE TABLE IF NOT EXISTS all_data(idx INTEGER PRIMARY KEY, title TEXT, age_min INTEGER, age_max INTEGER, area_code INTEGER, area_list TEXT, work TEXT, type TEXT, area TEXT, host TEXT, href TEXT)"
+    sql = 'CREATE TABLE IF NOT EXISTS all_data(idx INTEGER NOT NULL, title TEXT, age_min INTEGER, age_max INTEGER, area_code INTEGER, area_list TEXT, work TEXT, type TEXT, area TEXT, host TEXT, href TEXT, PRIMARY KEY("idx" AUTOINCREMENT))'
     sql2 = "CREATE TABLE IF NOT EXISTS all_data_Exist(idx INTEGER PRIMARY KEY AUTOINCREMENT,exist INTEGER, CONSTRAINT all_data_index FOREIGN KEY(idx) REFERENCES all_data(idx))"
 
     cursor.execute(sql)
@@ -25,16 +25,14 @@ def UpdateAlldata():
 
     index_num = []
 
-    if len(youth) > len(alldata):
-        InsertData(youth, alldata, index_num)
-    elif len(youth) < len(alldata):
-        DeleteData(youth, alldata, index_num)
+    InsertData(youth, alldata, index_num)
 
 
 def InsertData(youth, alldata, index_num):
     print("Insert Data")
     check = True
     # youth의 인덱스가 alldata보다 많을 경우 -> 새롭게 지원금 게시물이 올라온 경우
+
     for i in youth:
         check = True
         for j in alldata:
@@ -44,13 +42,17 @@ def InsertData(youth, alldata, index_num):
         if check == True:
             print(i)
             index_num.append(i[0])
-
+    '''
     for insertion in index_num:
         cursor.execute('SELECT * FROM youth WHERE index="%d" ')
         insert_data = cursor.fetchone()
         sql = 'INSERT INTO all_data VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
         print(cursor.execute(sql, insert_data))
-    print(index_num)
+    '''
+    cursor.execute(
+        "INSERT INTO all_data(title, age_min, age_max, area_code, area_list, work, type, area, host, href) SELECT * FROM youth LEFT OUTER JOIN all_data ON youth.title = all_data.title WHERE all_data.title IS NULL;"
+    )
+    cursor.commit()
 
 
 def DeleteData(youth, alldata, index_num):
@@ -73,5 +75,6 @@ def DeleteData(youth, alldata, index_num):
 
 
 checkdatabase()
+#cursor.execute("INSERT INTO all_data(title, age_min, age_max, area_code, area_list, work, type, area, host, href) SELECT title, age_min, age_max, area_code, area_list, work, type, area, host, href FROM youth")
 UpdateAlldata()
 conn.commit()
--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, request, jsonify
 import pandas as pd
-from flask_login import LoginManager
-from pandas import Series, DataFrame
 import sqlite3
-
 app = Flask(__name__)
+
+# 암호키 생성
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-login_manager = LoginManager()
-login_manager.init_app(app)
 
-
+# 데이터베이스 접근
+con = sqlite3.connect("database.db", check_same_thread=False)
+cur = con.cursor()
 
 # 루트
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-
-# db테이블확인
+# db 테이블 확인
 @app.route("/youth")
 def youth():
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
     query = cur.execute("SELECT * From youth")
     cols = [column[0] for column in query.description]
     result = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
-    con.close()
     #test = TodoPost()
     html = result.to_html(justify='center')
     return '<h1>Final_code_youth database.db - youth</h1><a href="/">홈화면으로 가기</br></br></a>' + html
 
 
-# 제목, 링크, 나이, 직업, 소득분위, 지원금TF json 코드 str형태로 리턴
+# 제목, 링크, 나이, 직업, 소득분위, 지원금 TF json 코드 str 형태로 리턴
 @app.route("/jsoncode")
 def TodoPost():
-    con = sqlite3.connect("database.db")
-    cur = con.cursor()
     query = cur.execute("SELECT * From youth")
     cols = [column[0] for column in query.description]
     race_result = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
@@ -45,10 +38,38 @@ def TodoPost():
     print(title)
     return js
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    try:
+        new_user = request.json
+        nickname = new_user.get('nickname')
+        id = new_user.get('id')
+        pw = new_user.get('pw')
+        birth = new_user.get('birth')
+        con.execute("INSERT INTO Person VALUES (NULL, ?, ?, ?, ?)", (nickname, id, pw, birth,))
+        con.commit()
+        return jsonify({"isOK": True})
+    except:
+        return jsonify({"isOK": False})
 
+@app.route("/login", methods=['POST'])
+def login():
+    try:
+        login_user = request.json
+        cur.execute("SELECT * FROM Person WHERE id = ? AND pw = ?", (login_user.get('id'), login_user.get('pw')))
+        user_info = cur.fetchone()
+        print(cur.fetchall())
+        return jsonify({"isValid": True, "nickname": user_info[1], "birth": user_info[4]})
+    except:
+        return jsonify({"isValid": False, "nickname": "", "birth": ""})
+
+@app.route("/User")
+def UserInfo():
+     result = cur.execute("SELECT * FROM Person")
+     cols = [column[0] for column in result.description]
+     result = pd.DataFrame.from_records(data=result.fetchall(), columns=cols)
+     html = result.to_html(justify='center')
+     return '<h1>Final_code_youth database.db - youth</h1><a href="/">홈화면으로 가기</br></br></a>' + html
 
 if __name__ == '__main__':
     app.run(debug=True)
